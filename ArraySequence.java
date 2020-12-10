dbc92f661fab82c8d3a64a53e7997effe58a399e
eabayev20 <eabayev20@gmail.com>
2020-12-09 22:45:21 -0500

    Second constructor done

--- ArraySequence.java
+++ ArraySequence.java
@@ -2,3 +2,22 @@ import java.util.NoSuchElementException;
 public class ArraySequence implements IntegerSequence{
   private int currentIndex;
   private int []data;
+  public ArraySequence (int [] other ) {
+    while (other.hasNext()) {
+      data.add(other.hasNext());
+    }
+
+  }
+  public ArraySequence (IntegerSequence otherseq) {
+    l = new int[otherseq.length()];
+    for(int i = 0; i < otherseq.length(); i++) {
+      l[i] = otherseq.next();
+    }
+    currentIndex = 0;
+    otherseq.reset();
+  }
+
+
+
+
+}
